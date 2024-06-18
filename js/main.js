fc9e97fb3d476d1e1af2dfc1f93f83e0fca588f5
joyce.perez <joyce.perez@nutricoach.pro>
2024-06-19 06:51:10 +0800

    fix: address auto-scroll to top

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ AOS.init({
 });
 
 (function ($) {
+
 	"use strict";
 
 	$(window).stellar({
@@ -15,11 +16,14 @@ AOS.init({
 		scrollProperty: 'scroll'
 	});
 
+
 	var fullHeight = function () {
+
 		$('.js-fullheight').css('height', $(window).height());
 		$(window).resize(function () {
 			$('.js-fullheight').css('height', $(window).height());
 		});
+
 	};
 	fullHeight();
 
@@ -34,35 +38,46 @@ AOS.init({
 	loader();
 
 	// Scrollax
-	// $.Scrollax();
+	$.Scrollax();
+
+
 
 	// Burger Menu
 	var burgerMenu = function () {
+
 		$('body').on('click', '.js-fh5co-nav-toggle', function (event) {
 
 			event.preventDefault();
+
 			if ($('#ftco-nav').is(':visible')) {
 				$(this).removeClass('active');
 			} else {
 				$(this).addClass('active');
 			}
 		});
+
 	};
 	burgerMenu();
 
-	// var onePageClick = function () {
-	// 	$(document).on('click', '#ftco-nav a[href^="#"]', function (event) {
-	// 		event.preventDefault();
 
-	// 		var href = $.attr(this, 'href');
-	// 		$('html, body').animate({
-	// 			scrollTop: $($.attr(this, 'href')).offset().top - 70
-	// 		}, 500, function () {
-	// 			// window.location.hash = href;
-	// 		});
-	// 	});
-	// };
-	// onePageClick();
+	var onePageClick = function () {
+
+
+		$(document).on('click', '#ftco-nav a[href^="#"]', function (event) {
+			event.preventDefault();
+
+			var href = $.attr(this, 'href');
+
+			$('html, body').animate({
+				scrollTop: $($.attr(this, 'href')).offset().top - 70
+			}, 500, function () {
+				// window.location.hash = href;
+			});
+		});
+
+	};
+
+	onePageClick();
 
 	$('nav .dropdown').hover(function () {
 		var $this = $(this);
@@ -82,44 +97,44 @@ AOS.init({
 	});
 
 	// scroll
-	// var scrollWindow = function () {
-	// 	$(window).scroll(function () {
-	// 		var $w = $(this),
-	// 			st = $w.scrollTop(),
-	// 			navbar = $('.ftco_navbar'),
-	// 			sd = $('.js-scroll-wrap');
+	var scrollWindow = function () {
+		$(window).scroll(function () {
+			var $w = $(this),
+				st = $w.scrollTop(),
+				navbar = $('.ftco_navbar'),
+				sd = $('.js-scroll-wrap');
 
-	// 		if (st > 150) {
-	// 			if (!navbar.hasClass('scrolled')) {
-	// 				navbar.addClass('scrolled');
-	// 			}
-	// 		}
-	// 		if (st < 150) {
-	// 			if (navbar.hasClass('scrolled')) {
-	// 				navbar.removeClass('scrolled sleep');
-	// 			}
-	// 		}
-	// 		if (st > 350) {
-	// 			if (!navbar.hasClass('awake')) {
-	// 				navbar.addClass('awake');
-	// 			}
+			if (st > 150) {
+				if (!navbar.hasClass('scrolled')) {
+					navbar.addClass('scrolled');
+				}
+			}
+			if (st < 150) {
+				if (navbar.hasClass('scrolled')) {
+					navbar.removeClass('scrolled sleep');
+				}
+			}
+			if (st > 350) {
+				if (!navbar.hasClass('awake')) {
+					navbar.addClass('awake');
+				}
 
-	// 			if (sd.length > 0) {
-	// 				sd.addClass('sleep');
-	// 			}
-	// 		}
-	// 		if (st < 350) {
-	// 			if (navbar.hasClass('awake')) {
-	// 				navbar.removeClass('awake');
-	// 				navbar.addClass('sleep');
-	// 			}
-	// 			if (sd.length > 0) {
-	// 				sd.removeClass('sleep');
-	// 			}
-	// 		}
-	// 	});
-	// };
-	// scrollWindow();
+				if (sd.length > 0) {
+					sd.addClass('sleep');
+				}
+			}
+			if (st < 350) {
+				if (navbar.hasClass('awake')) {
+					navbar.removeClass('awake');
+					navbar.addClass('sleep');
+				}
+				if (sd.length > 0) {
+					sd.removeClass('sleep');
+				}
+			}
+		});
+	};
+	scrollWindow();
 
 	var contentWayPoint = function () {
 		var i = 0;
